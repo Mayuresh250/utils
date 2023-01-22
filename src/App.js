5d12e50f5359a23daf41a52e2430aee13141ade5
Mayuresh250 <122156778+Mayuresh250@users.noreply.github.com>
2023-01-22 13:49:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import About from './components/About';
+import Alert from './components/Alert';
+import Navbar from './components/Navbar';
+import Text from './components/Text';
+/*import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";*/
 
-function App() {
+
+function App(){
+  
+  const [mode,setMode]=useState("light")
+  const [alert,setAlert]=useState(null)
+
+  const toggleMode=()=>{
+    if(mode==="light")
+    {
+      setMode("dark");
+      document.body.style.backgroundColor="#253c5e"
+      showAlert("Dark mode enabled.","Success")
+    }
+    else{
+      setMode("light");
+      document.body.style.backgroundColor="white"
+      showAlert("Light mode enabled.","Success")
+    }
+  }
+
+  const showAlert=(message,type)=>{
+    setAlert({
+    type:type,
+    msg:message
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+    {/* <Router>
+      <Routes>
+      <Route path="/">
+      </Route>
+      </Route>
+      </Routes>
+      <Route path="/about">
+    </Router>*/}
+
+    <Navbar title="Utils" showAlert={showAlert} toggleMode={toggleMode} mode={mode} home="Home" about="About"/>
+    <Alert alert={alert}/>
+    <div>
+              <Text mode={mode} showAlert={showAlert}/>
+              <About mode={mode}/>
     </div>
+  </>
+   
   );
 }
 
